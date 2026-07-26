fb15184efc37c9f0503643a60291788a28643f60
Alessio Deiana <adeiana@gmail.com>
2026-07-25 22:37:35 -0500

    Match authd test runner to CI

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,4 @@ set -eu
 cd "$(dirname "$0")"
 cargo fmt --all -- --check
 cargo test --workspace --locked
-cargo clippy --workspace --all-targets --all-features --locked -- -D warnings
+cargo clippy --workspace --locked
